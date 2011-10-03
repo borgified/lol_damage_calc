ab3b51c6813c40eb4e9682874d06eafc0d62e882
Fwiffo <borgified@gmail.com>
2011-10-03 04:12:34 -0700

    flat armor penetration calculations verified

--- calc.pl
+++ calc.pl
@@ -9,15 +9,28 @@ $flat_armor_reduction=25;
 $flat_armor_penetration=0;
 $percentage_armor_penetration=0;
 
+#percentage_armor_reduction calculations
 $effective_target_armor=($target_armor-($target_armor*$percentage_armor_reduction));
 
+#flat_armor_reduction calculations
 $effective_target_armor=($effective_target_armor-$flat_armor_reduction);
-
 if($effective_target_armor<0){
 	$damage_multiplier=(2-100/(100-($effective_target_armor)));
 }else{
 	$damage_multiplier=100/(100+$effective_target_armor);
 }
+
+#flat_armor_penetration calculations
+$reduced_armor_value=$effective_target_armor-$flat_armor_penetration;
+
+if($reduced_armor_value<0){
+	#no change (flat armor pen cannot reduce below zero)
+	$effective_target_armor=$effective_target_armor;
+}else{
+	$effective_target_armor=$reduced_armor_value;
+}
+
+
 
 $resulting_damage=$attacker_damage*$damage_multiplier;
 
