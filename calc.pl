2741a0fdc464d6d15e7ce38c79fe1659f2c49b09
Fwiffo <borgified@gmail.com>
2011-10-03 12:25:57 -0700

    ad calculations complete, need to test and ap calculations not started yet

--- calc.pl
+++ calc.pl
@@ -1,23 +1,37 @@
 #!/usr/bin/perl
 
-$target_armor=0;
+$target_armor=100;
 
-$attacker_damage=100;
+$attacker_damage=(100)*1.04;
+
+print "attacker damage: $attacker_damage\n";
+print "target armor: $target_armor\n";
 
 $percentage_armor_reduction=0;
-$flat_armor_reduction=25;
-$flat_armor_penetration=0;
+$flat_armor_reduction=0;
+$flat_armor_penetration=31+45;
 $percentage_armor_penetration=0;
 
 #percentage_armor_reduction calculations
 $effective_target_armor=($target_armor-($target_armor*$percentage_armor_reduction));
 
+if($percentage_armor_reduction>0){
+	print "effective armor due to percentage armor reduction: $effective_target_armor\n";
+}
+
 #flat_armor_reduction calculations
 $effective_target_armor=($effective_target_armor-$flat_armor_reduction);
+
+if($flat_armor_reduction>0){
+	print "effective armor due to flat armor reduction: $effective_target_armor\n";
+}
+
+#if target armor is already negative, remaining calculations have no effect
 if($effective_target_armor<0){
 	$damage_multiplier=(2-100/(100-($effective_target_armor)));
-}else{
-	$damage_multiplier=100/(100+$effective_target_armor);
+	$resulting_damage=$attacker_damage*$damage_multiplier;
+	print "resulting damage after damage mitigation from target's armor: $resulting_damage\n";
+	exit;
 }
 
 #flat_armor_penetration calculations
@@ -25,13 +39,31 @@ $reduced_armor_value=$effective_target_armor-$flat_armor_penetration;
 
 if($reduced_armor_value<0){
 	#no change (flat armor pen cannot reduce below zero)
-	$effective_target_armor=$effective_target_armor;
+	$effective_target_armor=0;
 }else{
 	$effective_target_armor=$reduced_armor_value;
 }
 
 
+if($flat_armor_penetration>0){
+	if($reduced_armor_value<0){
+		print "effective armor due to flat armor penetration ($reduced_armor_value wasted): $effective_target_armor\n";
+	}else{
+		print "effective armor due to flat armor penetration: $effective_target_armor\n";
+	}
+}
+
+
+#percentage_armor_penetration calculations
+$effective_target_armor=$effective_target_armor-($effective_target_armor*$percentage_armor_penetration);
+
+if($percentage_armor_penetration>0){
+	print "effective armor due to percentage armor penetration: $effective_target_armor\n";
+}
+
+#resulting damage calculations
+$damage_multiplier=100/(100+$effective_target_armor);
 
 $resulting_damage=$attacker_damage*$damage_multiplier;
 
-print $resulting_damage;
+print "resulting damage after damage mitigation from target's armor: $resulting_damage\n";
